7bb1b1b7632e7fdf83ebd6b3e632327f856426cd
Milos Puric <mp2059@cam.ac.uk>
2024-06-21 14:50:12 +0200

    calculator

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Welcome to the calculator!');
+const readline = require('readline-sync'); /// to prompt input
+
+console.log('Please enter a number: ');
+const str1 = readline.prompt(); // string input
+console.log('Please enter a number: ');
+const str2 = readline.prompt();
+
+console.log(+str1 + +str2);
+
