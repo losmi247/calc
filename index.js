42555416e9cfcc728d27eab205790415473afbf3
Milos Puric <mp2059@cam.ac.uk>
2024-06-21 15:03:15 +0200

    operators

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const readline = require('readline-sync'); /// to prompt input
 
-console.log('Please enter a number: ');
+console.log("Welcome to the calculator!");
+console.log("==========================");
+console.log("Please enter the operator: ");
+const strop = readline.prompt();
+console.log('Please enter the first number: ');
 const str1 = readline.prompt(); // string input
-console.log('Please enter a number: ');
+console.log('Please enter the second number: ');
 const str2 = readline.prompt();
 
-console.log(+str1 + +str2);
-
+switch(strop){
+	case "+":
+		console.log(+str1  + +str2);
+		break;
+	case "-":
+		console.log(str1 - str2);
+		break;
+	case "*":
+		console.log(str1 * str2);
+		break;
+	case "/":
+		console.log(str1 / str2);
+		break;	
+}
