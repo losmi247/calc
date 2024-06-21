0ce5ec4444cbd3b605c3650f8f11251202234514
Milos Puric <mp2059@cam.ac.uk>
2024-06-21 16:08:18 +0200

    many ops

--- index.js
+++ index.js
@@ -4,22 +4,30 @@ console.log("Welcome to the calculator!");
 console.log("==========================");
 console.log("Please enter the operator: ");
 const strop = readline.prompt();
-console.log('Please enter the first number: ');
-const str1 = readline.prompt(); // string input
-console.log('Please enter the second number: ');
-const str2 = readline.prompt();
+console.log(`How many times do you want to ${strop}? `);
+const times = parseInt(readline.prompt());
 
+let operands = new Array(times);
+for(let i = 0; i < times; i++){
+	console.log(`Please enter number ${i+1}: `);
+	const str = readline.prompt(); // string input
+	operands[i] = +str;
+}
+
+var res = 0;
 switch(strop){
 	case "+":
-		console.log(+str1  + +str2);
+		res = operands.reduce((acc,curr) => acc+curr, 0);
 		break;
 	case "-":
-		console.log(str1 - str2);
+		res = operands.slice(1).reduce((acc,curr) => acc-curr, operands[0]);
 		break;
 	case "*":
-		console.log(str1 * str2);
+		res = operands.reduce((acc,curr) => acc*curr, 1);
 		break;
 	case "/":
-		console.log(str1 / str2);
-		break;	
+		res = operands.slice(1).reduce((acc,curr) => acc/curr, operands[0]);
+		break;
 }
+
+console.log(`The answer is ${res}`);
